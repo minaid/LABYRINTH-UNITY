abc70b0f8d99a0e9cae6d1032421723b84f2bfad
minaid <33730811+minaid@users.noreply.github.com>
2020-12-10 11:24:40 +0200

    Code

---
+++ Move_to.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Move_to : MonoBehaviour {
+
+    // Use this for initialization
+
+    public Transform goal;
+    public Animator animator;
+    bool isWalking = false;
+    bool finished;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+
+    private void Update()
+    {
+        agent.destination = goal.position;
+        if (transform.position != goal.position && !isWalking && !finished)
+        {
+            Debug.Log("1");
+            animator.SetBool("IdleWalk", true);
+            isWalking = true;
+        }
+        if (transform.position.x == goal.position.x && transform.position.z == goal.position.z)
+        {
+            isWalking = false;
+            finished = true;
+            animator.SetBool("IdleWalk", false);
+            animator.SetBool("IdleStand", true);
+            GetComponent<NavMeshAgent>().isStopped = true;
+
+        }
+    }
+}
